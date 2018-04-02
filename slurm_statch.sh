f21c716983712283c012af8d20c6c81d31a31728
dongzhong <zhongdong0321@hotmail.com>
2018-04-02 15:46:38 -0400

    add slurm batch shell

---
+++ slurm_statch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "ompi"
+#default setting is to print only
+#   VmRSS: Resident set size.  Note that the value here is the sum of RssAnon, RssFile, and RssShmem.
+#    VmSize: Virtual memory size.
+
+salloc -k -n 240 -N 60 mpirun ./check_memory -a
+
