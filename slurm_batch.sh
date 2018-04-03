447c85b287cf6c5c0d6991d8f61ba34d40954dca
dongzhong <zhongdong0321@hotmail.com>
2018-04-02 20:43:32 -0400

    update for sbatch

--- slurm_batch.sh
+++ slurm_batch.sh
@@ -5,5 +5,5 @@ echo "ompi"
 #   VmRSS: Resident set size.  Note that the value here is the sum of RssAnon, RssFile, and RssShmem.
 #    VmSize: Virtual memory size.
 
-salloc -k -n 240 -N 60 mpirun ./check_memory -a
+salloc -k -n 240 -N 60 mpirun ./check_memory -l 1
 
